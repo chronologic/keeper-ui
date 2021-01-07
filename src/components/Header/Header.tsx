e6af4e6009a753f30f7263a03b27112e8e959608
AndreyDrozdov <AND@iMac.local>
2021-01-07 17:25:27 +0100

    ui implemented

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,78 @@
 import React from "react";
 import styled from "styled-components";
-import { Layout } from "antd";
+import { Layout, Typography, Menu, Dropdown, message, Space, Tag } from "antd";
+import { DownOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import logo from "../../img/logo.svg";
+import metamask from "../../img/metamask.svg";
+import trezor from "../../img/trezor.svg";
+import ledger from "../../img/ledger.svg";
+
+function handleMenuClick() {
+  message.info("Click on menu item.");
+  console.log("click");
+}
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item
+      key="1"
+      icon={<img src={metamask} className="wallet-icon" alt="metamask" />}
+    >
+      Metamask
+    </Menu.Item>
+    <Menu.Item
+      key="2"
+      icon={<img src={ledger} className="wallet-icon" alt="ledger" />}
+    >
+      Ledger
+    </Menu.Item>
+    <Menu.Item
+      key="3"
+      icon={<img src={trezor} className="wallet-icon" alt="trezor" />}
+    >
+      Trezor
+    </Menu.Item>
+  </Menu>
+);
+
+const { Text } = Typography;
 
 function Header() {
   return (
     <Layout.Header>
-      <StyledHeader>Header</StyledHeader>
+      <Space>
+        <img src={logo} className="App-logo" alt="logo" />
+        <ChronoLogic>
+          <Text>by ChronoLogic</Text>
+        </ChronoLogic>
+      </Space>
+      <ConnectWallet>
+        <Space>
+          <Tag icon={<CloseCircleOutlined />} color="error">
+            Network Disconnected
+          </Tag>
+          <Dropdown overlay={menu}>
+            <WalletButton>
+              Connect Wallet <DownOutlined />
+            </WalletButton>
+          </Dropdown>
+        </Space>
+      </ConnectWallet>
     </Layout.Header>
   );
 }
 
-const StyledHeader = styled.div`
-  color: white;
+const ConnectWallet = styled.div`
+  float: right;
+`;
+
+const WalletButton = styled.div`
+  height: 56px;
+`;
+
+const ChronoLogic = styled.div`
+  font-size: 0.9rem;
+  opacity: 0.5;
 `;
 
 export default Header;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,49 @@
 import React from "react";
 import styled from "styled-components";
-import { Layout } from "antd";
+import { Layout, Menu, Typography } from "antd";
+import home from "../../img/home.svg";
+import zendesk from "../../img/zendesk.svg";
+import discord from "../../img/discord.svg";
+
+const { Text, Link } = Typography;
 
 function Sidebar() {
   return (
     <Layout.Sider>
-      <StyledSidebar>Sidebar</StyledSidebar>
+      <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
+        <Menu.Item
+          key="1"
+          icon={<img src={home} className="menu-icon" alt="home" />}
+        >
+          Overview
+        </Menu.Item>
+        <Menu.Item
+          key="2"
+          icon={<img src={zendesk} className="menu-icon" alt="zendesk" />}
+        >
+          <Link
+            className="menu-link"
+            href="https://chronologic.zendesk.com/hc/en-us"
+            target="_blank"
+          >
+            Support
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+          key="3"
+          icon={<img src={discord} className="menu-icon" alt="discord" />}
+        >
+          <Link className="menu-link" href="url" target="_blank">
+            Discord
+          </Link>
+        </Menu.Item>
+      </Menu>
     </Layout.Sider>
   );
 }
 
-const StyledSidebar = styled.div`
-  color: white;
-  padding: 24px;
+const StyledLink = styled.div`
+  color: rgba(0, 0, 0, 0.85);
 `;
 
 export default Sidebar;
